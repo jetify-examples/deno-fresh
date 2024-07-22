7e067d609a31f2e123feba605f44aedbbb29b59d
Savil Srivastava <676452+savil@users.noreply.github.com>
2024-07-22 15:09:01 -0700

    start server on port 8080, and add devbox.json

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,3 +1,4 @@
 import { defineConfig } from "$fresh/server.ts";
 
-export default defineConfig({});
+// Use port 8080 to match the default port used by Jetify Cloud
+export default defineConfig({ server: { port: 8080 } });
